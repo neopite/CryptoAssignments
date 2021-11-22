6801882fda7cd88fe229c0ec17789f40ef3a9ffa
Yaroslav <Stamin4ik4@gmail.com>
2021-11-22 21:55:17 +0200

    add sha hash csv

--- Lab4/Program.cs
+++ Lab4/Program.cs
@@ -12,6 +12,11 @@ namespace Lab4
             CSVCreator csv = new CSVCreator(
                 "C:\\Users\\Stami\\RiderProjects\\Crypto\\Lab4\\Resources\\ResultHashedPasswords\\hashing-passwords1.csv",
                 new List<string>() {"hash", "salt"});
+
+            CSVCreator csv1 =
+                new CSVCreator(
+                    "C:\\Users\\Stami\\RiderProjects\\Crypto\\Lab4\\Resources\\ResultHashedPasswords\\hashing-passwords2.csv",
+                    new List<string>() {"hash", "salt"});
             var topPasswords = PseudoRandomPasswordGenerator.GetTopPasswords(20);
             var commonPasswordsList = PseudoRandomPasswordGenerator.GetCommonPasswords(1000);
             var random = PseudoRandomPasswordGenerator.GenerateRandomPasswords(1000);
@@ -21,9 +26,15 @@ namespace Lab4
             {
                 var hash = PasswordEncrypter.HashByMD5(smashedList[i]);
                 hashedList.Add(hash);
-                csv.AddRow(new List<string>(){hash});
+                csv.AddRow(new List<string>() {hash});
             }
-          
+
+            for (int i = 0; i < smashedList.Count; i++)
+            {
+                var hash = PasswordEncrypter.HashBySHA256(smashedList[i]);
+                hashedList.Add(hash);
+                csv1.AddRow(new List<string>() {hash});
+            }
         }
     }
 }
