8447a9b636e8c015fe47a7e86125f1470bd95f3c
Yaroslav <Stamin4ik4@gmail.com>
2021-11-25 17:33:12 +0200

    sep first lab solution

--- Lab4/Program.cs
+++ Lab4/Program.cs
@@ -26,23 +26,23 @@ namespace Lab4
             var commonPasswordsList = PseudoRandomPasswordGenerator.GetCommonPasswords(1000);
             var random = PseudoRandomPasswordGenerator.GenerateRandomPasswords(1000);
             var smashedList = topPasswords.Concat(commonPasswordsList).Concat(random).ToList();
-            for (int i = 0; i < smashedList.Count; i++)
-            {
-                var hash = PasswordEncrypter.HashByMD5(smashedList[i]);
-            }
-
-            for (int i = 0; i < smashedList.Count; i++)
-            {
-                var hash = PasswordEncrypter.HashBySHA256(smashedList[i]);
-                csv1.AddRow(new List<string>() {hash});
-            }
+            // for (int i = 0; i < smashedList.Count; i++)
+            // {
+            //     var hash = PasswordEncrypter.HashByMD5(smashedList[i]);
+            // }
+            //
+            // for (int i = 0; i < smashedList.Count; i++)
+            // {
+            //     var hash = PasswordEncrypter.HashBySHA256(smashedList[i]);
+            //     csv1.AddRow(new List<string>() {hash});
+            // }
             
             for (int i = 0; i < smashedList.Count; i++)
             {
                 var saltProvider = new SaltProvider();
                 var salt = saltProvider.GenerateSalt();
                 var hash = PasswordEncrypter.HashByArgon2(smashedList[i],salt);
-                csv2.AddRow(new List<string>() {hash});
+                csv2.AddRow(new List<string>() {hash,Convert.ToBase64String(salt)});
             }
         }
     }
