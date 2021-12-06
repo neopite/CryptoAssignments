2347afed532f9accaaab5e93b8de17d632f815c5
Yaroslav <Stamin4ik4@gmail.com>
2021-12-06 23:08:32 +0200

    end algorithm

--- Lap2/Program.cs
+++ Lap2/Program.cs
@@ -14,7 +14,7 @@ namespace Lab2
             {
                 if (i % 2 == 0 && i < str.Length - 1)
                 {
-                    result.Add($"{str[i]}{str[i+1]}");
+                    result.Add($"{str[i]}{str[i + 1]}");
                 }
             }
 
@@ -25,7 +25,7 @@ namespace Lab2
         {
             var byte1 = Convert.ToInt32(byte_str1, 16);
             var byte2 = Convert.ToInt32(byte_str2, 16);
-            var xorBytes= (byte) (byte1 ^ byte2);
+            var xorBytes = (byte) (byte1 ^ byte2);
             return xorBytes.ToString("X").PadLeft(2, '0');
         }
 
@@ -47,7 +47,18 @@ namespace Lab2
 
             return result;
         }
-        
+
+        public static string GetResult(List<string> array)
+        {
+            var sb = new StringBuilder();
+            foreach (var line in array)
+            {
+                sb.Append(String.Join("",Convert.FromHexString(line).Select(x => Convert.ToChar(x))));
+            }
+            return sb.ToString();
+
+        }
+
         static void Main(string[] args)
         {
             var str1_ciphered =
@@ -55,21 +66,19 @@ namespace Lab2
             var str2_ciphered = "2f0cdfe464344e8650edc59daac3504b1710d56b89dce5011e8c90f6";
 
             var supposedWord = "And lose the name of action.";
-            
+
             var result1 = CreateDuplets(str1_ciphered);
             var result2 = CreateDuplets(str2_ciphered);
-            
+
             var xorCipheredStrings = Xor(result1, result2);
-            
+
             var duplets1 = CreateDuplets(xorCipheredStrings);
             var duplets2 = CreateDuplets(Convert.ToHexString(Encoding.UTF8.GetBytes(supposedWord)));
 
             var array = GetXors(duplets1, duplets2);
-            
-            foreach (var VARIABLE in array)
-            {
-                Console.Write(VARIABLE + " ");
-            }
+
+            var result = GetResult(array);
+            Console.WriteLine(result);
         }
     }
 }
